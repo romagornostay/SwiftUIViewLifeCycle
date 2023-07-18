ad4fa697be086f4944cfd59e433d954b205697e0
Roman Gornostayev <roman.gornostayev@yandex.ru>
2023-07-18 22:13:45 +0700

    Initial Commit

---
+++ SwiftUIViewLifeCycle/SwiftUIViewLifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewLifeCycleApp.swift
+//  SwiftUIViewLifeCycle
+//
+//  Created by Роман Горностаев on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewLifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
